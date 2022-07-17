7ce9aec81c58357e786fcfca6f3d6153a5102d5b
Jose Miralles <josemiralles1991@gmail.com>
2022-07-16 22:30:38 -0400

    gql server working

--- GraphQL/project/src/app.ts
+++ GraphQL/project/src/app.ts
@@ -1,1 +1,27 @@
-console.log("Hello Kubernetes!");
+import express from "express";
+import {graphqlHTTP} from "express-graphql";
+import {buildSchema} from "graphql";
+
+const app = express();
+
+const schema = buildSchema(`
+type Query {
+    hello: String
+}
+`);
+
+const root = {
+    hello: () => {
+        return "Hello!!!!"
+    }
+}
+
+app.use("/graphql", graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root
+}));
+
+app.listen(5000, ()=> {
+    console.log("Running GQL at localhost:5000/graphql.");
+});
