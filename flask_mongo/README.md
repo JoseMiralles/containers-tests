f7d0b29f70d33c7aff0d72d2de29599bc27f8861
Jose Miralles <josemiralles1991@gmail.com>
2024-01-08 21:50:27 -0500

    Connected flask-api to supertokens idp

--- flask_mongo/README.md
+++ flask_mongo/README.md
@@ -5,8 +5,23 @@ Follow these steps: https://skaffold.dev/docs/quickstart/
 - Remember to add `type: LoadBalancer` to services that need to be accessed externally.
 - Run `minikube tunnel -p custom` to expose services.
 
+# Start cluster
+
+- `minikube start -p custom`
+- In the project dir: `skaffold dev`
+
+# Kubernetes dashboard
+
+Run `minikube dashboard -p custom`
+
 # Access Dev Services
 
 Mongo express: http://127.0.0.1:8081/
 API: http://127.0.0.1:5000/
 IDP: http://127.0.0.1:3567/
+IDP Dashboard: <FLASK_API_DOMAIN>/auth/dashboard
+
+
+# Supertokens API Swagger
+
+- https://app.swaggerhub.com/apis/supertokens/FDI/1.18.0
--- flask_mongo/api/app.py
+++ flask_mongo/api/app.py
@@ -1,6 +1,58 @@
-from flask import Flask
+from flask import Flask, abort
+
+from supertokens_python import init, InputAppInfo, SupertokensConfig, get_all_cors_headers
+from supertokens_python.recipe import emailpassword, session, dashboard
+from flask_cors import CORS 
+from supertokens_python.framework.flask import Middleware
+import os
+
+
+
+# SETUP AUTH
+
+init(
+    app_info=InputAppInfo(
+        app_name="api",
+        api_domain="localhost",
+        website_domain="localhost",
+        api_base_path="/auth",
+        website_base_path="/auth"
+    ),
+    # IDP_PORT=tcp://10.102.49.56:3567
+    supertokens_config=SupertokensConfig(
+        connection_uri=os.getenv("IDP_SERVICE_HOST") + ":" + os.getenv("IDP_SERVICE_PORT"),
+    ),
+    framework='flask',
+    recipe_list=[
+        session.init(), # initializes session features
+        emailpassword.init(),
+        dashboard.init()
+    ]
+)
 
 app = Flask(__name__)
+Middleware(app) # Supertokens middleware
+
+
+
+# CORS
+
+CORS(
+    app=app,
+    origins=[
+        "localhost"
+    ],
+    supports_credentials=True,
+    allow_headers=["Content-Type"] + get_all_cors_headers(),
+)
+
+
+
+@app.route('/', defaults={'u_path': ''})  
+@app.route('/<path:u_path>')  
+def catch_all(u_path: str):
+    abort(404)
+
 
 @app.route("/")
 def hello_world():
